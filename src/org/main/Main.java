1961f3bfcc116bb45b27f9c6efbffd2a94fbfbb0
Michal <sowinskim@interia.eu>
2018-02-24 08:40:29 +0100

    Inicjalizacja

---
+++ src/org/main/Main.java
@@ -0,0 +1,42 @@
+package org.main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+
+		List<String> nazwy = new ArrayList<>();
+		nazwy.add("jeden");
+		nazwy.add("dwa");
+		nazwy.add("trzy");
+		nazwy.add("cztery");
+		nazwy.add("piêæ");
+
+		File file = new File("dane.txt"); // tworzy obiekt pliku
+		BufferedWriter bufferedWriter = null;
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream(file);  // umozliwia zapisywanie danych
+			bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+			
+			for(String nazwa : nazwy) {
+				bufferedWriter.write(nazwa);
+				bufferedWriter.newLine();
+			}
+		} catch (IOException e) {
+			System.err.println("Problem z zapisem danych");
+		} finally {
+			try {
+				bufferedWriter.close();
+			} catch (IOException e) {
+				System.err.println("Problem z zamknieciem BufferedWriter");
+			}
+		}
+	}
+
+}
